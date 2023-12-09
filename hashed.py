eaefbd86e87c22527e14a7e1b23eeb302b7611d9
flyin_squirrel <gregory.horan@mohawkcollege.ca>
2023-12-09 13:53:25 -0500

    guidelines_update

--- hashed.py
+++ hashed.py
@@ -2,11 +2,13 @@
 """
 Cmd line interface for producing, saving and comparing file hash values.
 """
-
+# imports argparse module
 import argparse
+# imports haslib module 
 import hashlib
+# imports the three functions defined in makehash.py
 from makehash import fhash, savehsh, comphsh
-
+# defines main function, entry point for the module
 def main():
     """
     main function sets up argparse, 
@@ -16,56 +18,86 @@ def main():
     saves hash with -s option, 
     or compares hash with -c option.
     """
+# Please note: Blackbox AI was used to troubleshoot and build parts of this module.
+# Specifically - 
+# comphsh function's ability to compare the current hash value "hashdoc" to previously
+# stored hash values in hash.txt and return results 1 or 2 which are then passed to 
+# hashed.py. The idea to read the file "fpath" in chucks of 4096 bytes to ease memory use.
+#
+# Comments have been added to show functionality of each line in this module with special
+# emphasis placed on explaining the parts I had help with.
+
 # argparse
+
+# var parser is given new ArgumentParser object
     parser = argparse.ArgumentParser(
+# describes help for this, probably uneeded but I was paranoid and it works so it stayed
     description='Makes file hashes.')
-    
+# use argparse's add_argument method to add argument fpath as a string, describes help 
     parser.add_argument('fpath', 
     type=str, 
     help='Path to your target file.')
-    
+# use argparse's add_argument method to add argument alg as a string, describes help  
     parser.add_argument('alg', 
-    type=str, 
+    type=str,
+# alg arg has to be a recognized hashlib algorithm. list is defined in README.md
     choices=hashlib.algorithms_available, 
     help='Your desired hashing algorithm.')
 
-# make options mutually exclusive to avoid conflict  
+# make options mutually exclusive to avoid conflict by adding group 'group'
     group = parser.add_mutually_exclusive_group()
 
-# add option to save   
-    group.add_argument('-s', '--save', 
+# add option to save to group 'group' with add_argument method, describes help
+    group.add_argument('-s', '--save',
+# if save option is used, value stored as 'True'
     action='store_true', 
     help='Save hash to hash.txt')
 
-# option to compare
+# option to compare added to 'group' with add_argument method, store as 'True' if 
+# present, describe help
     group.add_argument('-c', '--compare', 
     action='store_true', 
     help='Compare hash with hash currently in hash.txt')
-    
+# parses arguments passed to script by user, passes them to 'args' as args.alg and
+# args.fpath, and if chosen one of args.save or args.compare
     args = parser.parse_args()
     
-# call fhash, incl. fpath and alg
+# call fhash function, incl. fpath and alg args, store as 'hashdoc'
     hashdoc = fhash(args.fpath, args.alg)
     
-# print hash
+# print hash with alg used to hash it
     print(f'The {args.alg} hash of your file is: {hashdoc}')
     
-# save hash
+# if save option was used, call savehsh function using fpath, alg and result
+# of fhash as 'hashdoc', print result saved 
     if args.save:
         savehsh(args.fpath, args.alg, hashdoc)
         print(f'Hash was saved to hash.txt')
         
-# compare hash
+# if compare option was used 
     if args.compare:
+# call comphsh function with args fpath, alg and results of fhash as 'hasdoc',
+# store as 'result'
         result = comphsh(args.fpath, args.alg, hashdoc)
+# comphash result was 'None' meaning there are either no lines in the file to check, 
+# the file does not exist, matches were found or permission is denied. ambiguous print
+# statement provided to cover all four potentials, but fhash and comphsh will inform 
+# the user if file doesn't exist or permission is denied. 
         if result is None:
             print(f'No hash found for {args.fpath}.')
+# comphsh returned 1, meaning the hash checked has the same fpath, alg, and hash as the 
+# last stored hash in hash.txt, print statement
         elif result == 1:
             print(f'This hash matches the last stored hash in hash.txt.')
+# comphsh returned 2, meaning the hash checked has the same fpath, alg, and hash as any 
+# of the other stored hashes in hash.txt that aren't the last one, print statement
         elif result == 2:
             print(f'This hash matches a previously stored hash in hash.txt.')
+# comphsh didn't return 1, 2 or None so the hash didn't match any stored hashes in
+# hashes.txt, print statement
         else:
             print(f'This hash differs from all previously stored hashes.')
-
+# common line in python modules, makes sure this only runs when run directly 
 if __name__ == "__main__":
+# calls main function
     main()
--- makehash.py
+++ makehash.py
@@ -5,58 +5,106 @@ Functions for producing, saving and comparing file hashes:
 - savehsh
 - comphsh
 """
-
+# import haslib module
 import hashlib
 
+# Please note: Blackbox AI was used to troubleshoot and build parts of this module.
+# Specifically:
+# comphsh function's ability to compare the current hash value "hashdoc" to previously
+# stored hash values and return results 1 or 2 which are then passed to hashed.py. 
+# The idea to iterate the file "fpath" in chucks of 4096 bytes to ease memory use.
+#
+# Comments have been added to show functionality of each line in this module with special
+# emphasis placed on explaining the parts I had help with.
+
+# define fhash function; args fpath and alg needed
 def fhash(fpath, alg):
     """
     fhash function takes a file path and hashing algorithm as inputs,
     reads provided file in binary and updates the hash,
     returns the hex version of the hash.
     """
+# new() function of hashlib creates new hash object and passes it to var 'hasher'
     hasher = hashlib.new(alg)
+# start a try block
     try:
-# read in binary
+# with open() opens the file for reading while allowing it to be auto closed,
+# read in binary 'rb' used, update() needs bytes so this is required, doc is
+# arbitrary name that file will be referenced as inside with block. 
         with open(fpath, 'rb') as doc:
 # read the file in chunks to allow for large files without affecting memory
+# for loop started with chunk var, iter() func uses lambda func to read 'doc'
+# 4096 bytes at a time and stops when it finds an empty byte string (b"") which
+# indicates the end of the file 
             for chunk in iter(lambda: doc.read(4096), b""):
+# passes the 4096 byte 'chunk' to 'hasher' for each loop, creating a binary
+# representation of the file stored in 'hasher'
                 hasher.update(chunk)
 # handle FileNotFound and Permission errors
+
+# start except block, handle a FileNotFoundError, related print statement,
+# None returned and passed to main()
     except FileNotFoundError:
         print(f"File {fpath} doesn't exist.")
         return None
+# start except block, handle PermissionError, related print statement,
+# None returned and passed to main()
     except PermissionError:
         print(f"You don't have permission to access file {fpath}.")
         return None
-# return hex value
+# return hex value of stored binary data currently in 'hasher' using hexdigest() method,
+# passed to main()
     return hasher.hexdigest()
-
+# define savehsh function; args fpath, alg and hashdoc (the var where main stores result
+# of fhash) required
 def savehsh(fpath, alg, hashdoc):
     """
     savehsh function takes a file path, hashing algorithm and hash value as inputs,
     appends to a file named hash.txt in the format fpath,alg,hashdoc.
     """
+# with open opens or creates and opens hash.txt and tells it to append, 'a', refers to
+# file as 'f' in this block.
     with open('hash.txt', 'a') as f:
+# write (append) vars fpath, alg, hashdoc to file 'f'(hash.txt) seperated by pipes
         f.write(f'{fpath}|{alg}|{hashdoc}\n')
-
+# define comphsh function; vars fpath, alg and hashdoc needed
 def comphsh(fpath, alg, hashdoc):
     """
     comphsh function takes a file path, hashing algorithm and hash value as inputs,
     searches for match in hash.txt,
     returns results.
     """
+# start try block
     try:
+# with open opens hash.txt in read mode, 'r', refers to it as 'f' in this block
+# with open will not create hash.txt in this func like savehsh does, see except below 
         with open('hash.txt', 'r') as f:
+# readlines() reads 'f' (hash.txt) and stores a list of strings for each line in 'lines'
             lines = f.readlines()
+# grabs the last line from hash.txt (lines[-1]), removes spaces(strip()), and splits
+# split() at pipes ('|'), storing the list of str in var last_entry, if hash.txt empty
+# returns None 
             last_entry = lines[-1].strip().split('|') if lines else None
+# initiates for loop that will loop over each line in 'lines'
             for line in lines:
+# for each line in 'lines', removes spaces(strip()), and splits split() at pipes ('|'),
+# storing the list of str in var entry
                 entry = line.strip().split('|')
+# if the first element of 'entry' matches current fpath var and the second matches the
+# alg var, continue
                 if entry[0] == fpath and entry[1] == alg:
+# if the current hash checked with -c matches the last stored hash in hash.txt, then
                     if entry == last_entry:
+# return 1, passed to main() as 'result'
                         return 1  # matches the last stored hash
+# otherwise it matches one line checked but it wasn't the last
                     else:
+# return 2, passed to main() as 'result'
                         return 2  # matches a previously stored hash
+# otherwise it didn't match any lines checked, return None, passed to main() as 'result'
             return None  # no hash found
+# start except block, handle a FileNotFoundError, related print statement,
+# None returned and passed to main()
     except FileNotFoundError:
         print("The hash.txt file doesn't exist.")
         return None
